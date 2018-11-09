d92336589e8301fdfcd7f5b7a3c795d827bad188
Ahmed Saad <ask@shopbox.com>
2018-11-10 00:16:47 +0200

    adding unit test

--- app/tests/fixtures/Hits/data/DbCreateNewGameRepositoryTest_case.php
+++ app/tests/fixtures/Hits/data/DbCreateNewGameRepositoryTest_case.php
@@ -13,16 +13,4 @@ return [
         "player" => 2,
         "game_id" => 1,
     ],
-    [
-        "id" => 3,
-        "value" => 3,
-        "player" => 1,
-        "game_id" => 1,
-    ],
-    [
-        "id" => 4,
-        "value" => 1,
-        "player" => 2,
-        "game_id" => 1,
-    ],
 ];
--- app/tests/unit/repositories/DbCreateHitsRepositoryTest.php
+++ app/tests/unit/repositories/DbCreateHitsRepositoryTest.php
@@ -7,7 +7,7 @@ use Codeception\Specify;
 use app\tests\fixtures\Games\GamesFixture;
 use app\tests\fixtures\Hits\HitsFixture;
 
-class DbCreateNewGameRepositoryTest extends TestCase
+class DbCreateHitsRepositoryTest extends TestCase
 {
     protected $createHitsRepository;
     use Specify;
@@ -36,10 +36,58 @@ class DbCreateNewGameRepositoryTest extends TestCase
             $player = 1;
             $game = 1;
             $createHitsRepository = Yii::$container->get('app\modules\game\repositories\CreateHitsRepositoryInterface', [$player, $game]);
-            $hit = $createHitsRepository->testCreateFirstHit();
+            $hit = $createHitsRepository->createFirstHit();
             $this->assertNotNull($hit);
             $this->assertTrue(is_a($hit, '\app\modules\game\models\Hits'));
             $this->assertTrue($hit->value > 1);
+        });
+    }
+
+    public function testCreateHit()
+    {
+        $this->specify("test positive creation for new hit (winning) with adding -1", function() {
+            $player = 1;
+            $game = 1;
+            $createHitsRepository = Yii::$container->get('app\modules\game\repositories\CreateHitsRepositoryInterface', [$player, $game]);
+            $lastHitValue = 4;
+            $hit = $createHitsRepository->createHit($lastHitValue);
+            $this->assertNotNull($hit);
+            $this->assertTrue(is_a($hit, '\app\modules\game\models\Hits'));
+            $this->assertTrue($hit->value * 3 - $lastHitValue == -1); // assert adding -1
+            $this->assertTrue($hit->value === 1); // last hit && right new value
+            $this->assertTrue($hit->isWinning($player)); // is winning for player 1
+            $this->assertFalse($hit->isWinning(2)); // the other player lose
+            $this->assertTrue($hit->game->status === 0); // the hit game became inactive
+        });
+
+        $this->specify("test positive creation for new hit (winning) with adding 0", function() {
+            $player = 1;
+            $game = 1;
+            $createHitsRepository = Yii::$container->get('app\modules\game\repositories\CreateHitsRepositoryInterface', [$player, $game]);
+            $lastHitValue = 3;
+            $hit = $createHitsRepository->createHit($lastHitValue);
+            $this->assertNotNull($hit);
+            $this->assertTrue(is_a($hit, '\app\modules\game\models\Hits'));
+            $this->assertTrue($hit->value * 3 - $lastHitValue == 0); // assert adding 0
+            $this->assertTrue($hit->value === 1); // last hit && right new value
+            $this->assertTrue($hit->isWinning($player)); // is winning for player 1
+            $this->assertFalse($hit->isWinning(2)); // the other player lose
+            $this->assertTrue($hit->game->status === 0); // the hit game became inactive
+        });
+
+        $this->specify("test positive creation for new hit (winning) with adding +1", function() {
+            $player = 1;
+            $game = 1;
+            $createHitsRepository = Yii::$container->get('app\modules\game\repositories\CreateHitsRepositoryInterface', [$player, $game]);
+            $lastHitValue = 2;
+            $hit = $createHitsRepository->createHit($lastHitValue);
+            $this->assertNotNull($hit);
+            $this->assertTrue(is_a($hit, '\app\modules\game\models\Hits'));
+            $this->assertTrue($hit->value * 3 - $lastHitValue == 1); // assert adding +1
+            $this->assertTrue($hit->value === 1); // last hit && right new value
+            $this->assertTrue($hit->isWinning($player)); // is winning for player 1
+            $this->assertFalse($hit->isWinning(2)); // the other player lose
+            $this->assertTrue($hit->game->status === 0); // the hit game became inactive
         });
     }
 }
